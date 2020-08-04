781efa6e14aafcc33870c4654bb3a36889475e39
Naratip Supakong <naratipcover@gmail.com>
2020-08-04 18:13:57 +0700

    study

--- start/routes.js
+++ start/routes.js
@@ -14,6 +14,21 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
+const Route = use('Route');
 
-Route.on('/').render('welcome')
+Route.on('/').render('home');
+Route.get("/login", (context)=>{
+    const {view,request,response} = context
+    const name = "Naratip"
+    const age = 20;
+    const friends= ["new","ko","han"];
+    const address = {
+        postcode:"10104",
+        country: "Thailand" ,
+    };
+    return view.render("login",{name,age,friends,address})
+})
+Route.get("/register", (context)=>{
+    const {view,request,response} = context
+    return view.render("register")
+})
