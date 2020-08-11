b7ef9ab98cfbf866f12eeab7433df2f4189755d2
Naratip Supakong <naratipcover@gmail.com>
2020-08-12 05:33:45 +0700

    add login to AuthController

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -1,63 +1,88 @@
 'use strict'
 const Database = use("Database");
+let token
+let usercurrent
 class AuthController {
-    async login ({view,request,response}) {
-        return view.render("login");
-        const users = await Database.select("*").from("users")
-        // .whereNot({age:20})
-        // .whereBetween('age',[18,32]);
-        // const name = "Ponlawat";
-        // const age = 20;
-        // const friends = ["Sue", "Bam", "Friend", "Fern"];
-        // const address = {
-        //     postcode: "10140",
-        //     country: "Thailand",
-        // }
-        return view.render("login", { users })
-    }
-    loginUser({view,request,response}){
-        const {username,password} =request.body
-        // const profile = request.body
-        // console.log(profile)
-        // return view.render("login")
-        return response.redirect("/login")
-    }
-    register = ({view}) => {
-        return view.render("register")
-    }
-    // async = *
-    async registerUser({request,response}){
-       const{email,password,username} = request.body;
-       await Database.from("users").insert({email,password,username});
-    //    await Database.insert({email,password}).into("users")
-    //    yield
-    //    await = yield 
-       return response.redirect("/login");
-   }
-    
+   
 // ....................................................................
+
+    login = ({view}) => {
+        return view.render("login")
+    } 
+    async loginUser ({view,request,response}) {
+        const{username,password} = request.body;
+        const datauser = await Database
+        .select("*")
+        .from("registers")
+        .where({ usernames:username,passwords:password});
+
+        if(datauser.length){
+          
+            usercurrent=datauser ;
+            return response.redirect('/home')
+
+        }
+        else{
+            
+            return response.redirect('/login')
+        }
+        return response.redirect("/login");
+    }
+
     home ({view,request,response}) {
         return view.render("home");
     }
-   
-    comment ({view,request,response}) {
-        return view.render("comment");
+    comment = ({view}) => {
+        return view.render("comment")
+    } 
+    async commentUser ({request,response}) {
+        const{write_post,write_comment,date} = request.body;
+        await Database.insert({write_post,write_comment,date}).into("comments");
+        return response.redirect("/comment");
     }
+    
+    
+
+    register = ({view}) => {
+        return view.render("register")
+    } 
+    async registerUser ({request,response}) {
+        const{usernames,passwords,confirmPassword} = request.body;
+        await Database.insert({usernames,passwords,confirmPassword}).into("registers");
+
+        return response.redirect("/login");
+    }
+
+    
+
     post ({view,request,response}) {
         return view.render("post");
     }
+
     regionNorthern({view,request,response}) {
         return view.render("regionNorthern");
     }
+
     regionSouthern({view,request,response}) {
         return view.render("regionSouthern");
     }
+
     regionCentral({view,request,response}) {
         return view.render("regionCentral");
     }
+
     regionIsaan({view,request,response}) {
         return view.render("regionIsaan");
     }
+
+    loginProject({view,request,response}) {
+        return view.render("loginProject");
+    }
+
+    index({view,request,response}) {
+        return view.render("index");
+    }
+
 }
 
 module.exports = AuthController;
