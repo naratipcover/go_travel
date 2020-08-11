fc1427c1e11be5829f7788ffeb46b8bf277c11f6
Naratip Supakong <naratipcover@gmail.com>
2020-08-12 06:17:50 +0700

    add routes (loginProject,comment,index) delete routes (/api/rigieter)

--- start/routes.js
+++ start/routes.js
@@ -18,19 +18,26 @@ const Route = use('Route');
 // Route.on('/').render('home');
 Route.get("/login", "AuthController.login");
 Route.post("/login","AuthController.loginUser");
+
 Route.get("/register", "AuthController.register");
-Route.post("/api/register","AuthController.registerUser");
 Route.post("/register","AuthController.registerUser");
+// Route.post("/api/register","AuthController.registerUser");
+
 
 
 
 Route.get("/home", "AuthController.home");
+Route.get("/loginProject", "AuthController.loginProject");
+
 Route.get("/comment", "AuthController.comment");
+Route.post("/comment", "AuthController.commentUser");
+
 Route.get("/post", "AuthController.post");
 Route.get("/regionNorthern", "AuthController.regionNorthern");
 Route.get("/regionSouthern", "AuthController.regionSouthern");
 Route.get("/regionCentral", "AuthController.regionCentral");
 Route.get("/regionIsaan", "AuthController.regionIsaan");
+Route.get("/index", "AuthController.index");
 
 
 
